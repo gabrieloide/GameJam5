bd3abbc30292ace5b0ff42c7de2655d802c29a15
Gabriel Fuentes <gabrielfuentes7312@gmail.com>
2023-02-04 17:31:22 -0400

    Cambios menores a playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,76 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public enum FamilyState
+{
+    Dad,
+    Mom,
+    Son
+}
 public class PlayerController : MonoBehaviour
 {
+    public FamilyState familyState = FamilyState.Dad;
+    public int indexFamilyState;
+    Dictionary<int,FamilyState> tableFamily = new Dictionary<int, FamilyState>();
+
     public Rigidbody theRB;
     public float moveSpeed, jumpForce;
-
     private Vector2 moveInput;
-
     public LayerMask whatIsGround;
     public Transform groundPoint;
     private bool isGrounded;
-
     public Animator anim;
-
     public SpriteRenderer show;
-
-    public Sprite fowards;
-
-    public Sprite Backwards;
-
-    public Animator flipAnim;
-
-    // Start is called before the first frame update
-    void Start() 
+    public Sprite fowards, Backwards;
+    private void Start()
     {
-        
+        tableFamily.Add(0, FamilyState.Dad);
+        tableFamily.Add(1, FamilyState.Mom);
+        tableFamily.Add(2, FamilyState.Son);
     }
-
-    // Update is called once per frame 
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
+        ChangeFamilyMember();
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveInput.Normalize();
-
-        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
 
         anim.SetFloat("moveSpeed", theRB.velocity.magnitude);
 
-        Debug.Log(moveInput);
-
         RaycastHit hit;
-        if(Physics.Raycast(groundPoint.position, Vector3.down, out hit, .3f, whatIsGround))
+        bool Hit =Physics.Raycast(groundPoint.position, Vector3.down, out hit, .3f, whatIsGround)? isGrounded = true: isGrounded = false;
 
-        {
-            isGrounded = true;
-        } else
-        {
-            isGrounded = false;
-        }
         if(Input.GetButtonDown("Jump") && isGrounded)
-
         {
             theRB.velocity += new Vector3(0f, jumpForce, 0f);
         }
-
         anim.SetBool("onGround", isGrounded);
+        Vector3 n = moveInput.x < -0.1f  ? transform.localScale = new Vector3(-1, 1, 1) : transform.localScale = new Vector3(1, 1, 1);
 
-        if (moveInput.x < -.1f) 
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        } else if (moveInput.x > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
         if (moveInput.y > 0.1)
-        {
             show.sprite = Backwards;
-        }
         else
             show.sprite = fowards;
     }
-
+    private void FixedUpdate()=> theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
+    void ChangeFamilyMember()
+    {
+        if (true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                indexFamilyState++;
+                familyState = tableFamily[indexFamilyState];
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                indexFamilyState--;
+                familyState = tableFamily[indexFamilyState];
+            }
+        }
+    }
 }
