50e33efaa16ea3e56b410737024e5328629daa32
Gabriel Fuentes <gabrielfuentes7312@gmail.com>
2023-02-05 15:23:09 -0400

    cambio

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -5,27 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class OpenDoor : MonoBehaviour
 {
-    public bool openDoor;
-    public string NextLevel;
-    private void Update()
-    {
-        if (openDoor && GameManager.instance.hasKey)
-        {
-            SceneManager.LoadScene(NextLevel);
-        }
-    }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && GameManager.instance.hasKey)
         {
-            openDoor = true;
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            openDoor = false;
+            Destroy(gameObject);
         }
     }
 
